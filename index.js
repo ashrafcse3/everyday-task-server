57ce3fc84fa16fc8767675445f53aac0a5aa0bfb
Ashraful Alam <ashrafcse3@gmail.com>
2022-12-28 17:27:45 +0000

    tasks get method added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = 4000 || process.env.PORT;
@@ -14,10 +15,26 @@ app.get('/', (req, res) => {
     res.send('Root testing request with nodemon 3');
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p9scd9o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const tasksCollection = client.db("everyday-task").collection("tasks");
+
+
+
 async function run() {
     try {
         app.post('/tasks', async (req, res) => {
-            console.log(req);
+            const result = await tasksCollection.insertOne(req.body);
+
+            res.send(result);
+        })
+
+        app.get('/tasks', async (req, res) => {
+            const query = {};
+            const tasks = await tasksCollection.find(query).toArray();
+
+            res.send(tasks);
         })
     }
     finally { }
